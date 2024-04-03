ceaa50e940b98d144f81589040b97ffe358a64a6
kristiine-dev <165931610+kristiine-dev@users.noreply.github.com>
2024-04-03 19:19:34 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,47 @@
+
+public class Main {
+  public static void main(String[] args) {    
+
+    String season = "qwerty";
+
+    if (season == "winter") {
+      System.out.println("Wear a warm jacket!");
+    }
+    else if (season == "spring") {
+      System.out.println("Wear a T-shirt!");
+    }
+    else if (season == "summer") {
+      System.out.println("Wear a swimming suite!");
+    }
+    else if (season == "autumn") {
+      System.out.println("Wear a rain coat!");
+    }
+    else {
+      System.out.println("I do not recongnize this season!");
+    }
+
+
+    
+
+    double temp = -10.23;
+
+    if (temp <= 5) {
+      System.out.println("Wear super warm");
+    }
+    else if (temp <= 15) {
+      System.out.println("Wear warm");
+    }
+    else if (temp <= 30) {
+      System.out.println("Wear normal");
+    }
+    else {
+      System.out.println("You need cooling");
+    }
+
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
